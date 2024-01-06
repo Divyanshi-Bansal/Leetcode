ab9f218e4cbb4cf7923df87258158abb8784e44f
Divyanshi-Bansal <54770459+Divyanshi-Bansal@users.noreply.github.com>
2024-01-07 02:26:25 +0530

    Time: 0 ms, Memory: 41.9 MB - LeetHub

---
+++ plus-one/plus-one.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+        int length = digits.length;
+        if(digits[length-1] != 9){//[1,2,3,4,5,....(but not 9 at the last)]
+            digits[length-1]++;
+            return digits;
+        }
+        if(length == 1){//[9]
+            int[] newArr = {1,0};
+            return newArr;
+        }
+        
+        //[1,2,3,2,9,9], [9,9]
+        int temp = 0;
+        for(int i = length-1 ; i>=0 ; i--){
+            if(digits[i]==9){
+                digits[i] = 0;
+                temp=1;
+            }else{
+                digits[i]++;
+                temp=0;
+                break;
+            }
+        }
+        if(temp == 1){
+            int[] newArr = new int[length+1];
+            newArr[0] = 1;
+            System.arraycopy(digits, 0, newArr, 1, digits.length);
+            return newArr;
+        }else{
+            return digits;
+        }
+    }
+}
