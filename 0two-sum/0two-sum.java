22a744e7ebc13cbc7ad80b3cffd3fb3718f38a31
Divyanshi-Bansal <54770459+Divyanshi-Bansal@users.noreply.github.com>
2024-01-08 01:51:21 +0530

    Time: 3 ms, Memory: 44.6 MB - LeetHub

--- 0two-sum/0two-sum.java
+++ 0two-sum/0two-sum.java
@@ -1,16 +1,30 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {//[2,7,11,15]
         
         int length = nums.length;
-        for (int i = 0 ; i < length ; i++){
-            for(int j = i+1; j<length; j++){
-                int sum = nums[i] + nums[j];
-                if(sum == target){
-                    int[] ans = {i,j};
-                    return ans;
-                }
+        //o(n2)
+        // for (int i = 0 ; i < length ; i++){
+        //     for(int j = i+1; j<length; j++){
+        //         int sum = nums[i] + nums[j];
+        //         if(sum == target){
+        //             int[] ans = {i,j};
+        //             return ans;
+        //         }
+        //     }
+        // }
+        // return nums;
+        Map<Integer, Integer> map = new HashMap<>();  //{7=0,2=1,-2=2,6=3}
+        for(int i=0;i<length;i++){
+            
+            if(map.containsKey(nums[i])){
+                int[] ans = {i,map.get(nums[i])};
+                return ans;
+            }else{
+                map.put(target-nums[i],i);
             }
         }
+        
         return nums;
+        
     }
 }
