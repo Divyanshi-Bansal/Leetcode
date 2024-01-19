07d067863b559ab7d4c9f38238d3f8916a27617e
Divyanshi-Bansal <54770459+Divyanshi-Bansal@users.noreply.github.com>
2024-01-19 19:09:28 +0530

    Time: 71 ms, Memory: 18.4 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,19 +4,25 @@ class Solution:
         l = len(nums) #3
         temp = list(range(l)) #3
         index = 0
-        for i in range(l-1): #0,1
-            if(nums[i] != nums[i+1]):
-                temp[index] = nums[i] #temp = [1]
-                index += 1 #1
+#         for i in range(l-1): #0,1
+#             if(nums[i] != nums[i+1]):
+#                 temp[index] = nums[i] #temp = [1]
+#                 index += 1 #1
         
-        temp[index] = nums[l-1] #temp = [1,2]
-        index += 1  #2
+#         temp[index] = nums[l-1] #temp = [1,2]
+#         index += 1  #2
         
-        # nums[:len(temp)] = temp[:len(temp)]
-        for i in range(len(temp)):
-            nums[i] = temp[i]
-        return index
-        
+#         for i in range(len(temp)):
+#             nums[i] = temp[i]
+#         return index
+
+        for i in range(l-1): #[1,1,2,3,3,4,5,5]
+            if nums[i] != nums[i+1]:
+                nums[index] = nums[i] #nums = [1,2,3,4], index = 4
+                index += 1
+        nums[index] = nums[l-1] # index = 4, nums=[1,2,3,4,5]
+        index += 1 #index=5
+        return index #5
         
             
                 
