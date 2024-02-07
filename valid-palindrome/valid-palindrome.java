54356c08f00a2632482cb3d15136db4adb742fd3
Divyanshi-Bansal <54770459+Divyanshi-Bansal@users.noreply.github.com>
2024-02-07 12:18:46 +0530

    Time: 127 ms, Memory: 46.1 MB - LeetHub

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,6 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String s1 = s.replaceAll("[^a-zA-Z0-9]","");
+        // String s1 = s.replaceAll("[^a-zA-Z0-9]","");
+        String s1 = "";
+        for(char c : s.toCharArray()){
+            if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')){
+                s1 += String.valueOf(c);
+            }
+        }
         StringBuilder sb = new StringBuilder(s1);
         sb.reverse();
         String rev = sb.toString();
@@ -10,5 +16,7 @@ class Solution {
         }
         
         return false;
+        
+        
     }
 }
